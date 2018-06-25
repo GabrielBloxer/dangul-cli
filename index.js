49aac2fc3936911aea3c92d7473099bc7c93cff3
Datto <30291671+GabrielBloxer@users.noreply.github.com>
2018-06-25 12:14:25 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log("Hello World!");
+// const cmder = require('commander');
+
+// cmder.arguments('<cmd>')
+//     .option('-pn --pname', 'The projet name')
+//     .action((cmd) => {
+//         console.log(cmd);
+        
+//         if (cmd === "new") {
+//             console.log(`projectName: %s`, cmder.pname);
+//         }
+//     })
+//    .parse(process.argv);
